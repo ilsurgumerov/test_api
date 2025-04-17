00c1133aa423d5e0381e3a2bb5c026df59e7b548
Klowest <nosowq10@gmail.com>
2025-04-17 23:38:26 +0300

    Анализ данных

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,7 @@ def custom_mapping(X):
     X = X.copy()
 
     X['Gender'] = X['Gender'].map({'Male': 1, 'Female': 0})
-    X['Vehicle_Age'] = X['Vehicle_Age'].map({'< 1 Year': 0, '1-2 Year': 1, '> 2 Years': 2})
+    X['Vehicle_Age'] = X['Vehicle_Age'].map({'< 1 Year': 0, '1-2 Year': 1, '> 2 Years': 1})
     X['Vehicle_Damage'] = X['Vehicle_Damage'].map({'Yes': 1, 'No': 0})
 
     return X
